b4faf3ac546173dc2aa352af25d56135fdc2e0af
Alec Larsen <hello@alec.ninja>
2021-11-26 09:54:13 -0800

    Add basic scripts

---
+++ script/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd no-titlebar-when-maximized@alec.ninja
+zip ../no-titlebar-when-maximized@alec.ninja.zip *
---
+++ script/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf ~/.local/share/gnome-shell/extensions/no-titlebar-when-maximized@alec.ninja
+cp -r no-titlebar-when-maximized@alec.ninja ~/.local/share/gnome-shell/extensions
+
